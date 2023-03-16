735b91172d3ccc2e57cb9b59f853c5f27b894965
vermavikasakash <vikas26061995@gmail.com>
2023-03-16 20:34:44 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-//import App from './JSON_Creation/states/App';
-import App from './JSON_Creation/counter_appl/App';
+import App from './JSON_Creation/counter_appl2/App';
+
 
 createRoot(document.getElementById("root")).render(<App/>);
