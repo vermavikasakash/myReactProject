19abc9acdbe060e67ed2bb6d18f8c79117463ac6
vermavikasakash <vikas26061995@gmail.com>
2023-03-18 09:37:13 +0530

    first

---
+++ src/JSON_Creation/event1/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import {Component} from "react"
+class App extends Component{
+    constructor(){
+        super()
+        this.state={username:""}
+    }
+    componentDidMount(){
+        let one=document.querySelector("button")
+        one.addEventListener("click",()=>{
+            this.setState({username:this.state.username="KGF"})
+        })
+    }
+    render(){
+        return(<>
+        <h1>{this.state.username}</h1>
+        <button>{Change}</button>
+        </> )
+    }
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-
-import App from './JSON_Creation/toast/App';
-
+import App from './JSON_Creation/event1/App';
 
 createRoot(document.getElementById("root")).render(<App/>);
